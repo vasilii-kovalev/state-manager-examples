87fff6f50e037e83243569be394323e44f5dcbc8
Vasilii Kovalev <vasilii.kovalev95@yandex.ru>
2025-09-06 08:55:16 +0500

    Sort classes

--- react/src/routes/__root.tsx
+++ react/src/routes/__root.tsx
@@ -16,13 +16,13 @@ export const Route = createRootRoute({
 		return (
 			<main>
 				<header
-					className="flex items-center gap-col-4 b-0 b-be-1px b-solid p-4 pos-fixed inset-inline-0 bg-white inset-bs-0"
+					className="pos-fixed inset-inline-0 flex items-center gap-col-4 b-0 b-be-1px b-solid bg-white p-4 inset-bs-0"
 				>
 					<Navigation/>
 				</header>
 
 				<section
-					className="p-4 m-bs-13"
+					className="m-bs-13 p-4"
 				>
 					<Outlet/>
 				</section>
